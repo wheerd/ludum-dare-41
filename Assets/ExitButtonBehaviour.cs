50ae0bd65e78f4c32a563e1de48c6a47364df50c
Wheerd <admin@wheerd.de>
2018-04-21 19:13:28 +0200

    Added exit button

---
+++ Assets/ExitButtonBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ExitButtonBehaviour : MonoBehaviour {
+
+	void Start()
+	{
+		GetComponent<Button>().onClick.AddListener(OnClick);
+	}
+
+	void OnClick()
+	{
+		Application.Quit ();
+	}
+}
